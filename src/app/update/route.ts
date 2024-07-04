ab11cea92c2096fc4bc6857666f289a343272ec0
Sergii Novak <novak.sera@gmail.com>
2024-07-04 13:23:18 +0200

    Added deploy

--- src/app/update/route.ts
+++ src/app/update/route.ts
@@ -1,20 +1,6 @@
 import { exec } from "child_process";
 import { NextResponse } from "next/server";
 
-// To handle a GET request to /api
-export async function GET(request: any) {
-  try {
-    await runWebhook();
-    return NextResponse.json({ message: "Success" });
-  } catch (error) {
-    console.error("Error executing webhook:", error);
-    return NextResponse.json({ message: "Internal Server Error" });
-  }
-
-  // Do whatever you want
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
-}
-
 const runWebhook = async () => {
   return new Promise((resolve, reject) => {
     exec(
@@ -32,3 +18,13 @@ const runWebhook = async () => {
     );
   });
 };
+// To handle a GET request to /api
+export async function GET(request: any) {
+  try {
+    await runWebhook();
+    return NextResponse.redirect("/");
+  } catch (error) {
+    console.error("Error executing webhook:", error);
+    return NextResponse.redirect("/");
+  }
+}
