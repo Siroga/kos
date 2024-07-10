7c869f5aaae49cc5c44f4cfc79e8884e219fd5d3
Sergii Novak <novak.sera@gmail.com>
2024-07-10 19:21:15 +0200

    Reapply "pok"
    This reverts commit 3f955b5dcb5b7721680bb169e9c2821d634fd64b.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -99,3 +99,85 @@ export const playSound = () => {
     audio.play();
   } catch (e) {}
 };
+
+export const btnAddPok = (val: IItem, isTv: boolean = false) => {
+  let newItems = document.getElementById("new-items") as any;
+
+  let newDiv = document.createElement("button") as any;
+
+  newDiv.className = val.type === MenuTypeEnum.KITCHEN ? "red" : "yellow";
+
+  let newScore = val !== null && val.number! ? val.number! : 1;
+
+  newDiv.id = newScore;
+
+  newDiv.appendChild(document.createTextNode(newScore.toString()));
+  newDiv.appendChild(document.createElement("br"));
+  const sp1 = document.createElement("div");
+  sp1.innerText = val.count! > 1 ? val.name! + "-" + val.count! : val.name!;
+
+  newDiv.appendChild(sp1);
+
+  const sp = document.createElement("div");
+  sp.innerText = val.comment ? val.comment! : " ";
+  sp.className = "comment";
+  newDiv.appendChild(sp);
+  newDiv.onclick = () => {
+    const item: IItem = {
+      number: val.number,
+      type: val.type,
+      status:
+        val.status === "New"
+          ? "Progress"
+          : val.status === "Progress"
+          ? "Ready"
+          : "Done",
+    };
+    socket.emit("update_item", item);
+  };
+
+  newItems.appendChild(newDiv);
+  //   if (isTv) {
+  //     if (val.status === "Ready") {
+  //       readyItems.appendChild(newDiv);
+  //     } else if (val.status === "Progress" || val.status === "New") {
+  //       inprogressItems.appendChild(newDiv);
+  //     }
+  //   } else {
+  //     if (val.status === "New") {
+  //       newItems.appendChild(newDiv);
+  //     } else if (val.status === "Ready") {
+  //       readyItems.appendChild(newDiv);
+  //     } else if (val.status === "Progress") {
+  //       inprogressItems.appendChild(newDiv);
+  //     }
+  //   }
+
+  return newScore;
+};
+
+export const addItemsToPagePok = (
+  items: IItem[],
+  type: MenuTypeEnum,
+  sound: boolean = true,
+  isTv = false
+): number => {
+  let newItems = document.getElementById("new-items") as any;
+  newItems.innerHTML = "";
+
+  let lastIndex = 0;
+
+  items.forEach((item) => {
+    if (item.number! > lastIndex) {
+      lastIndex = item.number!;
+    }
+
+    if (type === item.type || type === MenuTypeEnum.ALL) {
+      btnAddPok(item, isTv);
+      if (item.status === "New" && item.sound && sound) {
+        playSound();
+      }
+    }
+  });
+  return lastIndex;
+};
