97c003a9afdada7bdad80d2d7f31a5eab3c5c835
Sergii Novak <novak.sera@gmail.com>
2025-05-27 00:19:31 +0200

    Added printer

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ let items = [];
 let lastIndex = 0;
 let pizzaCount = 0;
 let todayDate = new Date();
+const fs = require("fs");
 
 app.prepare().then(() => {
   const httpServer = createServer(handler);
@@ -35,6 +36,10 @@ app.prepare().then(() => {
       });
     }, 500);
 
+    setInterval(() => {
+      io.emit("priter_status", checkConnection()); // Broadcast the message to all connected clients
+    }, 10000); // every hour
+
     socket.on("connect", (message) => {
       console.log("connect");
     });
@@ -55,6 +60,7 @@ app.prepare().then(() => {
         lastIndex: lastIndex,
         pizzaCount: pizzaCount,
       }); // Broadcast the message to all connected clients
+      print(message);
 
       items = items.map((item) => {
         item.sound = false;
@@ -104,3 +110,51 @@ app.prepare().then(() => {
       console.log(`> Ready on http://${hostname}:${port}`);
     });
 });
+
+function checkConnection() {
+  const devicePath = "/dev/rfcomm0";
+  if (!fs.existsSync(devicePath)) {
+    return false;
+  }
+  return true;
+}
+
+function print(message) {
+  const bigNumber = lastIndex.toString();
+  const now = new Date();
+  const pad = (n) => n.toString().padStart(2, "0");
+  const dateString =
+    `${pad(now.getDate())}.${pad(now.getMonth() + 1)}.${now.getFullYear()} ` +
+    `${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}`;
+  //const dateString = now.toLocaleString();
+  // Create Buffer with commands and text:
+  const escposCommands = Buffer.concat([
+    Buffer.from([0x1b, 0x40]), // ESC @ - initialize
+    Buffer.from([0x1b, 0x61, 0x01]), // ESC a 1 - center alignment
+    Buffer.from(dateString + "\n", "ascii"),
+    Buffer.from([0x1d, 0x21, 0x77]), // GS ! 0x77 - max font size (8x8)
+    Buffer.from(bigNumber, "ascii"), // text (number)
+    Buffer.from("\n\n", "ascii"), // new lines (feed paper)
+    Buffer.from([0x1d, 0x21, 0x00]),
+    Buffer.from(message.name + "-" + message.count, "ascii"), // new lines (feed paper)
+    Buffer.from("\n", "ascii"), // new lines (feed paper)
+    Buffer.from(message.comment, "ascii"), // new lines (feed paper)
+    Buffer.from("\n\n\n\n\n", "ascii"), // new lines (feed paper)
+    Buffer.from([0x1d, 0x56, 0x00]), // GS V 0 - cut paper
+  ]);
+  // Open device file
+  const devicePath = "/dev/rfcomm0";
+  if (!fs.existsSync(devicePath)) {
+    return;
+  }
+
+  const stream = fs.createWriteStream(devicePath);
+  stream.on("error", (err) => {
+    console.error("Print error:", err.message);
+  });
+  stream.on("open", () => {
+    stream.write(escposCommands, () => {
+      stream.end();
+    });
+  });
+}
