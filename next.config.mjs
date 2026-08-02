916eae9ed7ea53222d5f43d9aaa4cfa5ea4f152a
Sergii Novak <novak.sera@gmail.com>
2026-08-03 00:28:36 +0200

    Small update

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 const nextConfig = {
   reactStrictMode: false,
   transpilePackages: ["react-router-typing"],
-  allowedDevOrigins: ["kemp.local:8888", "localhost:8888", "kemp.local", "192.168.0.172:8888", "kemp.tail0b1ff7.ts.net", "kemp.tail0b1ff7.ts.net:8888"],
+  allowedDevOrigins: ["*"],
   turbopack: {
     root: __dirname,
   },
